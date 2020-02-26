70deb14d30574e008ff3471535bf2ee6e2d931f1
Jufren Barasa <28448785+Jufrenbarasa@users.noreply.github.com>
2020-02-27 05:32:13 +0700

    Login Page

---
+++ Login.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pemrograman_Visual
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        
+        private void btnLogin(object sender, RoutedEventArgs e)
+        {
+            var Home = new Home();
+            Home.Show();
+            this.Close();
+        }
+    }
+}
